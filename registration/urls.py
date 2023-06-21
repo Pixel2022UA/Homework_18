f34150d3a8dfe1f38e9546412733be77b89b202b
Pixel2022UA <zelenskiy2013@gmail.com>
2023-06-21 14:59:58 +0300

    update

--- registration/urls.py
+++ registration/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("registration/", views.signup, name="registration"),
+    path(
+        "activate/<str:id>/<str:token>/",
+        views.activate,
+        name="activate",
+    ),
     path("logout/", views.logout_view, name="logout"),
     path("login/", views.login_view, name="login")
 ]
